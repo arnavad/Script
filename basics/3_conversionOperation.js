e2a64339ef1557d25465a3c420c7bca929751968
arnavad <funnyvideos5495@gmail.com>
2025-05-05 15:22:05 +0530

    tr

--- basics/3_conversionOperation.js
+++ basics/3_conversionOperation.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "Aditya"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
     //  console.log(booleanIsLoggedIn);
